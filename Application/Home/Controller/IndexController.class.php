65c5b7a8096eb2be3b48d56a38f4cbec1c1e6f94
Your Name <you@example.com>
2015-06-26 14:27:51 +0800

    edit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -167,9 +167,9 @@ class IndexController extends BaseController
     {
         $key = '%'.I('get.q').'%';
         $listRows = 5;
-        $count = M('news')->where(array('title'=>['LIKE',$key],'status' => 1, 'user_id' => 0))->count();
+        $count = M('news')->where(array('title'=>['LIKE',$key],'status' => 1, 'user_id' => 0, 'news_id'=>0))->count();
         $p = new Page($count, $listRows);
-        $list = M('news')->where(array('title'=>['LIKE',$key],'status' => 1, 'user_id' => 0))->limit($p->firstRow . ',' . $p->listRows)->select();
+        $list = M('news')->where(array('title'=>['LIKE',$key],'status' => 1, 'user_id' => 0, 'news_id'=>0))->limit($p->firstRow . ',' . $p->listRows)->select();
 
         foreach ($list as $key => $value) {
             if ($value['news_id']) {
