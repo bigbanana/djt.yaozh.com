5ec53a6318e3777657acc79eebb7a06a7c4a8bd2
Your Name <you@example.com>
2015-06-26 13:44:21 +0800

    edit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -28,6 +28,7 @@ class IndexController extends BaseController
         $this->last_news = $last_news;
         $this->last_active = $last_active;
 
+        $this->users = M('User')->order('sort desc')->limit(5)->select();
         $this->display();
     }
 
