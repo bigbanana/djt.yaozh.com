45d3689d1a054a64172579db0fb1a73b0df74c2f
Your Name <you@example.com>
2015-07-22 09:50:53 +0800

    edit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -28,7 +28,7 @@ class IndexController extends BaseController
         $this->last_news = $last_news;
         $this->last_active = $last_active;
 
-        $this->users = M('User')->order('sort desc')->limit(5)->select();
+        $this->users = M('User')->order('sort desc')->limit(15)->select();
         $this->display();
     }
 
@@ -103,7 +103,7 @@ class IndexController extends BaseController
     {
         $this->condition = 2;
         $this->banner = M('adv')->where(array('adv_id' => '2', 'status' => 1))->getField('pic');
-        $this->users = M('User')->order('sort desc')->limit(5)->select();
+        $this->users = M('User')->order('sort desc')->select();
         $this->display();
     }
 
