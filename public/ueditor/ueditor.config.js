c9284e41f5485f6093115d1e0ed09e0d630089fb
zhangpeng <skxtay@gmail.com>
2015-08-06 11:51:22 +0800

    zz

--- public/ueditor/ueditor.config.js
+++ public/ueditor/ueditor.config.js
@@ -33,7 +33,7 @@
         , serverUrl: "/upload"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
-        , toolbars: [['undo','redo','|' ,'bold','italic','underline','|','superscript', 'subscript','|','forecolor','backcolor','removeformat','insertorderedlist','insertunorderedlist','|','simpleupload','insertimage','|','selectall','cleardoc','paragraph','|','fontfamily', 'fontsize','|','justifyleft','justifycenter','justifyright','justifyjustify','|','horizontal','preview','fullscreen','formula']]
+        , toolbars: [['undo','redo','|' ,'bold','italic','underline','|','superscript', 'subscript','|','forecolor','backcolor','removeformat','insertorderedlist','insertunorderedlist','|','simpleupload','insertimage','|','selectall','cleardoc','paragraph','|','fontfamily', 'fontsize','|','justifyleft','justifycenter','justifyright','justifyjustify','|','horizontal','preview','fullscreen','formula','source']]
 
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
