dfdce5d2791ced1b09a0a6a99ae1d716094527eb
wbygit <875678022@qq.com>
2015-08-07 15:57:57 +0800

    视屏功能

--- Application/Admin/Controller/ActiveController.class.php
+++ Application/Admin/Controller/ActiveController.class.php
@@ -44,7 +44,8 @@ class ActiveController extends BaseController
 
     }
 
-    public function edit(){
+    public function edit()
+    {
         $id = I('get.id');
         $this->info = M('news')->find($id);
         $this->display('Active/add');
@@ -59,9 +60,10 @@ class ActiveController extends BaseController
             $this->success('删除失败');
         }
     }
-
+// =======================相册====================//
     // 相册列表
-    public function lists(){
+    public function lists()
+    {
         // 查询
         $this->_list(M('pic'));
 
@@ -69,7 +71,8 @@ class ActiveController extends BaseController
     }
 
     // 添加相册
-    public function addpic(){
+    public function addpic()
+    {
         if (IS_POST) {
             // $_POST['type'] = 2;
             // $_POST['status'] = 1;
@@ -146,7 +149,8 @@ class ActiveController extends BaseController
     }
 
     // 相册删除
-    public function deletepic(){
+    public function deletepic()
+    {
         $id = I('get.id');
         if (M('pic')->where(array('id' => $id))->delete() && M('picshow')->where(array('pid' => $id))->delete()) {
             $this->success('删除成功');
@@ -155,5 +159,52 @@ class ActiveController extends BaseController
         }
     }
 
+// =======================视频====================//
+
+    public function liststv()
+    {
+        $this->_list(M('video'));
+        $this->display();
+    }
+
+    public function addtv()
+    {
+        if (IS_POST) {
+            $_POST['time'] = time();
+            if (I('post.id')) {
+                if (M('video')->save(I('post.'))) {
+                    $this->success('修改成功');
+                } else {
+                    $this->error('修改失败');
+                }
+            } else {
+                if (M('video')->add(I('post.'))) {
+                    $this->success('添加成功');
+                } else {
+                    $this->error('添加失败');
+                }
+            }
+        } else {
+            $this->display();
+        }
+
+    }
+
+    public function edittv()
+    {
+        $id = I('get.id');
+        $this->info = M('video')->find($id);
+        $this->display('Active/addtv');
+    }
+
+    public function deletetv()
+    {
+        $id = I('get.id');
+        if (M('video')->where(array('id' => $id))->delete()) {
+            $this->success('删除成功');
+        } else {
+            $this->success('删除失败');
+        }
+    }
 
 }
