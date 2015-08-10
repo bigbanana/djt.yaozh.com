72cb5cb728ce6a28380775b3e84a25b6f73dabb0
zhangpeng <skxtay@gmail.com>
2015-08-10 15:05:27 +0800

    zz

--- public/ueditor/ueditor.config.js
+++ public/ueditor/ueditor.config.js
@@ -19,7 +19,7 @@
      * 因此，UEditor提供了针对不同页面的编辑器可单独配置的根路径，具体来说，在需要实例化编辑器的页面最顶部写上如下代码即可。当然，需要令此处的URL等于对应的配置。
      * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
      */
-    var URL = window.UEDITOR_HOME_URL || "/club/public/ueditor/" || getUEBasePath();
+    var URL = window.UEDITOR_HOME_URL || "/public/ueditor/" || getUEBasePath();
 
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
@@ -30,10 +30,10 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        //, serverUrl: "/upload"
+        , serverUrl: "/upload"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
-        , toolbars: [['undo','redo','|' ,'bold','italic','underline','|','superscript', 'subscript','|','forecolor','backcolor','removeformat','insertorderedlist','insertunorderedlist','|','simpleupload','insertimage','|','selectall','cleardoc','paragraph','|','fontfamily', 'fontsize','|','justifyleft','justifycenter','justifyright','justifyjustify','|','horizontal','preview','fullscreen','formula','source']]
+        , toolbars: [['undo','redo','|' ,'bold','italic','underline','|','superscript', 'subscript','|','forecolor','backcolor','removeformat','insertorderedlist','insertunorderedlist','|','simpleupload','insertimage','|','selectall','cleardoc','paragraph','|','fontfamily', 'fontsize','|','justifyleft','justifycenter','justifyright','justifyjustify','|','horizontal','preview','fullscreen','formula']]
 
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
